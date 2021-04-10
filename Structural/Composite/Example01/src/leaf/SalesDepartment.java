a11c8abe2f75849527fafddfa9187ef2ffb3fbb5
aturk <aturk@netas.com.tr>
2021-04-10 14:30:02 +0300

    composite

---
+++ Structural/Composite/Example01/src/leaf/SalesDepartment.java
@@ -0,0 +1,35 @@
+package leaf;
+
+import component.Department;
+
+public class SalesDepartment implements Department {
+
+    private Integer id;
+    private String name;
+
+    public SalesDepartment(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printDepartmentName() {
+        System.out.println(getClass().getSimpleName());
+    }
+}
